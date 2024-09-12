937bb4fd69957e99bf8f64ddfbe791ca94b38e1b
agneswanjiru <167208815+agneswanjiru@users.noreply.github.com>
2024-09-12 17:48:45 +0300

    Add files via upload

---
+++ database 3.sql
@@ -0,0 +1,40 @@
+CREATE TABLE client ( clientidNo INT,client name ,  branchidNo INT,primary key (branchidNo));
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(400 ,'Dunmore Highschool', 2);
+SELECT * from client;
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(401 ,'LackawanaCountry' ,2);
+SELECT * from client;
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(402 ,'FedEx' ,3);
+SELECT * from client;
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(403 ,'JohnDalyLawLLC', 3);
+SELECT * from client;
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(404, 'Scranton Whitepages', 2);
+SELECT * from client;
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(405 ,'Times Newspaper', 3);
+SELECT * from client;
+
+
+ insert into client( clientidNo ,client  ,  branchidNo )
+VALUES(406 ,'FedEx' ,2);
+SELECT * from client;
+
+
+
+
+
+
+
+
+
+
