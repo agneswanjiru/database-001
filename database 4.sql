eac0e270b76004461b7a6cf2f4ea8d302cf6141b
agneswanjiru <167208815+agneswanjiru@users.noreply.github.com>
2024-09-12 17:49:29 +0300

    Add files via upload

---
+++ database 4.sql
@@ -0,0 +1,26 @@
+create table mgr ( branchidNo INT,branch name,mgridNo INT,mgrstartdate DATE,primary key (mgridNo));
+
+insert into mgr(branchidNo,branch,mgridNo,mgrstartdate)
+VALUES(1 ,'Corporate', 100 ,'2006-02-09');
+SELECT * from mgr;
+
+insert into mgr( branchidNo,branch,mgridNo,mgrstartdate )
+VALUES(2 ,'Scranton' ,102 ,'1992-04-06');
+SELECT * from mgr;
+
+insert into mgr( branchidNo,branch,mgridNo,mgrstartdate )
+VALUES(3 ,'Stamford' ,106 ,'1998-02-13');
+SELECT * from mgr;
+
+
+
+
+
+
+
+
+
+1 Corporate 100 2006-02-09
+2 Scranton 102 1992-04-06
+3 Stamford 106 1998-02-13
+
