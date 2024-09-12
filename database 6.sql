c9f129f53bfdb6945ffa6b1a9170507e1dd770d1
agneswanjiru <167208815+agneswanjiru@users.noreply.github.com>
2024-09-12 17:50:14 +0300

    Add files via upload

---
+++ database 6.sql
@@ -0,0 +1,32 @@
+CREATE TABLE supplier (branchid  INT, supplier name ,supply_type name,primary key (supply_type));
+
+insert into supplier (branchid , supplier  ,supply_type);
+VALUES(2,'Hammer Mill','Paper');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(2 ,'Uni-ball Writing' ,'Utensils');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(3, 'Patriot Paper', 'Paper');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(2 ,'J.T. Forms & Labels', 'Custom Forms');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(3, 'Uni-ball', 'Writing Utensils');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(3, 'Hammer Mill', 'Paper');
+SELECT * from supplier;
+
+insert into supplier (branchid , supplier  ,supply_type)
+VALUES(3 ,'Stamford Lables', 'Custom Forms');
+SELECT * from supplier;
+
+
+
