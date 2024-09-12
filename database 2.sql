8b8020b326300abbec0e645fa76f83c233dc9544
agneswanjiru <167208815+agneswanjiru@users.noreply.github.com>
2024-09-12 17:45:09 +0300

    Add files via upload

---
+++ database 2.sql
@@ -0,0 +1,45 @@
+CREATE TABLE students ( empidNo INT,first name ,last name,  birthdate INT  ,sex CHAR,salary INT,superidNo INT , branchidNo INT);
+
+ insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(100, 'David', 'Wallace', 1967_11_17, 'M', 250000, NULL,1);
+
+SELECT * from students;
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(101, 'Jan' ,'Levinson' ,1961_05_11 ,'F', 110000, 100, 1);
+SELECT * from students;
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(102, 'Michael', 'Scott', 1964_03_15, 'M', 75000, 100,2);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(103, 'Angela', 'Martin',1964_03_15 , 'F',75000 , 100,1);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(104, 'Kelly', 'Kapoor', 1980_02_05, 'F',55000, 102, 2);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(105, 'Stanley', 'Hudson', 1958_02_19, 'M', 69000, 102, 2);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(106, 'Josh', 'Porter', 1969_09_05, 'M', 78000 ,100, 3);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(107, 'Andy' ,'Bernard', 1973_07_22, 'M', 65000, 106, 3);
+SELECT * from students
+
+insert into students(empidNo,first  ,last ,  birthdate ,sex ,salary ,superidNo , branchidNo )
+VALUES(108 ,'Jim', 'Halpert', 1978_10_01, 'M', 71000, 106, 3);
+SELECT * from students
+
+
+
+
+
+
+
