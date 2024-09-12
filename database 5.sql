0d92bbe949942b0e770394e31b9c8c43ac5b0897
agneswanjiru <167208815+agneswanjiru@users.noreply.github.com>
2024-09-12 17:49:54 +0300

    Add files via upload

---
+++ database 5.sql
@@ -0,0 +1,51 @@
+CREATE TABLE employe ( empidNo INT,clientidNo INT ,totalsales INT primary key (totalsales));
+
+insert into employe ( empidNo ,clientidNo  ,totalsales )
+VALUES(105, 400, 55000);
+SELECT * from employe ;
+
+insert into employe ( empidNo ,clientidNo  ,totalsales )
+VALUES(102, 401, 267000);
+SELECT * from employe;
+
+insert into employe ( empidNo ,clientidNo  ,totalsales )
+VALUES(108 ,402 ,22500);
+SELECT * from employee;
+
+insert into employe ( empidNo ,clientidNo  ,totalsales )
+VALUES(107 ,403, 5000);
+SELECT * from employe;
+
+insert into employe ( empidNo,clientidNo ,totalsales )
+VALUES(108, 403, 12000);
+SELECT * from employe;
+
+insert into employe ( empidNo,clientidNo ,totalsales )
+VALUES(105, 404, 33000);
+SELECT * from employe;
+
+insert into employe ( empidNo ,clientidNo  ,totalsales )
+VALUES(107, 405, 26000);
+SELECT * from employe;
+
+
+
+insert into employee ( empidNo ,clientidNo  ,totalsales )
+VALUES(102, 406, 15000);
+SELECT * from employee;
+
+insert into employee ( empidNo ,clientidNo ,totalsales )
+VALUES(105, 406, 130000);
+SELECT * from employee;
+
+
+
+
+
+
+
+
+
+
+
+
